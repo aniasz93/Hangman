f0ea16f7384ef54728e3310bc97012add5f0eeac
Anna Szymańska <aniasz93@gmail.com>
2014-04-01 00:31:59 +0200

    new class

--- Hangman/GameForm.cs
+++ Hangman/GameForm.cs
@@ -11,12 +11,20 @@ namespace Hangman
 {
     public partial class GameForm : Form
     {
+        #region Variables
+
+        Word word = new Word();
+        string wordToGuess = "";
+        string hiddenWord = "";
+        List<string> letters = new List<string>();
+
+        #endregion
+
         #region Constructors
 
         public GameForm()
         {
             InitializeComponent();
-            init();
         }
 
         #endregion
@@ -26,12 +34,12 @@ namespace Hangman
         private void okBtn_Click(object sender, EventArgs e)
         {
             string letter = letterTB.Text;
-            bool isAddedLetter = IsLetterAddedToUsedLetterList(letter);
+            letters.Add(letter);
 
-            if (!isAddedLetter)
-            {
-                usedLetterLabel.Text += letter + " ";
-            }
+            bool isAddedLetter = word.IsLetterUsedAlready(letters, letter);
+
+            hiddenWord = word.DiscoveryLetters(hiddenWord, wordToGuess, letter, isAddedLetter, usedLetterLabel);
+            guessingWordLabel.Text = hiddenWord;
         }
 
         // set stopwatch
@@ -40,13 +48,6 @@ namespace Hangman
 
         private void GameForm_Load(object sender, EventArgs e)
         {
-            // name choosen category
-            string category = categoryLabel.Text = GameOptionsForm.Category;
-
-            // draw word
-            string wordToGuess = DrawWord(category);
-            guessingWordLabel.Text = wordToGuess;
-
             // stopwatch
             if (sec == null)
             {
@@ -54,94 +55,23 @@ namespace Hangman
                 sec.SynchronizingObject = this;
                 sec.Elapsed += new System.Timers.ElapsedEventHandler(SecTick);
             }
-            
+
             sec.Start();
+
+
+            // name choosen category
+            string category = categoryLabel.Text = GameOptionsForm.Category;
+
+            // draw word
+            wordToGuess = word.DrawWord(category);
+
+            // hide word
+            guessingWordLabel.Text = hiddenWord = word.HideWord(wordToGuess);
         }
 
         #endregion
 
         #region Methods
-
-        private void init()
-        {
-            categoryLabel.Text = GameOptionsForm.Category;
-        }
-
-        // checking if letters was added to used letters list
-        private bool IsLetterAddedToUsedLetterList(string letter)
-        {
-            bool isLetter = false;
-
-            int i = 0;
-            do
-            {
-                if (usedLetterLabel.Text.Length != 0)
-                {
-                    if (letter != usedLetterLabel.Text.Substring(i, 1))
-                    {
-                        i += 2;
-                    }
-                    else
-                    {
-                        isLetter = true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            } while (i < usedLetterLabel.Text.Length && !isLetter);
-
-            return isLetter;
-        }
-
-        // drawing letter to guessing
-        private string DrawWord(string cat)
-        {
-            string fileName = @"D:\Projekty\Hangman\Categories\" + cat + ".txt";
-            List <string> words = new List<string>();
-            string word = "";
-            Random rand = new Random();
-            int wordNumb = 0;
-
-            int i = 0;
-
-            if (System.IO.File.Exists(fileName))
-            {
-                System.IO.StreamReader objReader = new System.IO.StreamReader(fileName);
-                
-                do
-                {
-                    words.Add(objReader.ReadLine());
-                    i++;
-                } while (objReader.Peek() != -1);
-
-                objReader.Close();
-            }
-            else
-            {
-                MessageBox.Show("File not found " + fileName);
-            }
-
-            wordNumb = rand.Next(i);
-
-            int j = 0;
-            do
-            {
-                if (j == wordNumb)
-                {
-                    word = words[j];
-                    break;
-                }
-                else
-                {
-                    j++;
-                }
-            } while (j < i);
-
-            return word;
-        }
-
 
         // stopwatch ticking
         private void SecTick(object sender, System.Timers.ElapsedEventArgs e)
---
+++ Hangman/Word.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    class Word
+    {
+        #region Methods
+
+        // checking if letter was used already
+        public bool IsLetterUsedAlready(List<string> letters, string letter)
+        {
+            bool isLetter = false;
+
+            int i = 0;
+            while (i < letters.Count() && !isLetter)
+            {
+                if (letters[i] == letter)
+                {
+                    isLetter = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return isLetter;
+        }
+
+        //public bool IsLetterOnUsedLettersList(string letter, Label usedLetters, string wordToGuess)
+        //{
+        //    bool isLetter = false;
+
+        //    int i = 0;
+        //    do
+        //    {
+        //        if (wordToGuess.Substring(i, 1) != letter)
+        //        {
+        //            i++;
+        //        }
+        //        else
+        //        {
+        //            isLetter = true;
+        //        }
+        //    } while (i < wordToGuess.Count() && !isLetter);
+
+        //    int j = 0;
+        //    while (j < usedLetters.Text.Length && !isLetter)
+        //    {
+        //        if (usedLetters.Text.Length != 0)
+        //        {
+        //            if (usedLetters.Text.Substring(j, 1) != letter)
+        //            {
+        //                j += 2;
+        //            }
+        //            else
+        //            {
+        //                isLetter = true;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
+
+        //    return isLetter;
+        //}
+
+        // drawing letter to guessing
+        public string DrawWord(string cat)
+        {
+            string fileName = @"D:\Projekty\Hangman\Categories\" + cat + ".txt";
+            List<string> words = new List<string>();
+            string word = "";
+            Random rand = new Random();
+            int wordNumb = 0;
+
+            int i = 0;
+
+            if (System.IO.File.Exists(fileName))
+            {
+                System.IO.StreamReader objReader = new System.IO.StreamReader(fileName);
+
+                do
+                {
+                    words.Add(objReader.ReadLine());
+                    i++;
+                } while (objReader.Peek() != -1);
+
+                objReader.Close();
+            }
+            else
+            {
+                MessageBox.Show("File not found " + fileName);
+            }
+
+            wordNumb = rand.Next(i);
+
+            int j = 0;
+            do
+            {
+                if (j == wordNumb)
+                {
+                    word = words[j];
+                    break;
+                }
+                else
+                {
+                    j++;
+                }
+            } while (j < i);
+
+            return word;
+        }
+
+        // replace guessing word with '_'
+        public string HideWord(string word)
+        {
+            int numbOfLetters = word.Count();
+
+            for (int i = 0; i < numbOfLetters; i++)
+            {
+                if (word[i] != ' ')
+                {
+                    word = word.Replace(word[i], '?');
+                }
+                else
+                {
+
+                }
+            }
+
+            return word;
+        }
+
+        // discovery of letters
+        public string DiscoveryLetters(string hiddenWord, string wordToGuess, string letter, bool isLetterUsed, Label usedLetterLabel)
+        {
+            if (isLetterUsed)
+            {
+                return hiddenWord;
+            }
+
+            for (int i = 0; i < wordToGuess.Count(); i++)
+            {
+                if (wordToGuess.Substring(i, 1) == letter)
+                {
+                    hiddenWord = hiddenWord.Replace(hiddenWord.Substring(i, 1), letter);
+                    isLetterUsed = true;
+                }
+            }
+
+            if (!isLetterUsed)
+            {
+                usedLetterLabel.Text += letter + " ";
+            }
+
+            return hiddenWord;
+        }
+
+        #endregion
+    }
+}
